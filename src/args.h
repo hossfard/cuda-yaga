54d567a9a6cba56f2ba619a44ea2de421544bbe5
Hossein Fadaifard <fadaifard@gmail.com>
2023-04-08 06:55:25 -0500

    Move parsing of args to args.h

--- src/args.h
+++ src/args.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 #include <cuda_runtime.h>
 
 
@@ -45,6 +46,93 @@ struct args{
        && (input.rep_count > 0)
        && (maxDeviceId < deviceCount);
   }
+
+  static
+  void
+  usage(std::string const& exec, std::ostream &stream){
+     stream << "Usage: "
+            << exec << " args\n";
+     stream << "args:\n"
+            << "   -m <int> Row count of matrix A\n"
+            << "   -n <int> Column count of matrix A\n"
+            << "   -k <int> Column count of matrix B\n"
+            << "   -r <int> dgemm repetition count to measure flops\n"
+            << "   -p <str> ('fp64', 'fp64') data type - defaults to fp32\n"
+            << "   -d <str> comma sep list of device ids, indexed at zero, to run concurrently run dgemm on\n"
+            << "   -i <int> Number of iterations to of dgemm to perform\n"
+            << "   [-o] <str> optional filename to write all data. If not .csv, will write in json\n";
+  }
+
+  static
+  args
+  parse(int argc, char *argv[]){
+     args ret;
+     std::string const exec(argv[0]);
+
+     while (--argc > 0){
+       std::string arg((++argv)[0]);
+       if (arg == "-m"){
+         std::string const val((++argv)[0]);
+         ret.m = std::stoi(val);
+         --argc;
+       }
+       else if (arg == "-n"){
+         std::string const val((++argv)[0]);
+         ret.n = std::stoi(val);
+         --argc;
+       }
+       else if (arg == "-k"){
+         std::string const val((++argv)[0]);
+         ret.k = std::stoi(val);
+         --argc;
+       }
+       else if (arg == "-d"){
+         std::string const val((++argv)[0]);
+         auto vals = split(val, ',');
+         std::for_each(vals.begin(), vals.end(), [&ret](std::string const& v){
+            ret.device_ids.push_back(std::stoi(v));
+         });
+         --argc;
+       }
+       else if (arg == "-p"){
+         std::string const val((++argv)[0]);
+         if (val == "fp32"){
+           ret.dtype = precision_t::fp32;
+         }
+         else if (val == "fp64"){
+           ret.dtype = precision_t::fp64;
+         }
+         --argc;
+       }
+       else if (arg == "-i"){
+         std::string const val((++argv)[0]);
+         ret.iter_count = std::stoi(val);
+         --argc;
+       }
+       else if (arg == "-r"){
+         // number of times to repeat while measuring duration
+         std::string const val((++argv)[0]);
+         ret.rep_count = std::stoi(val);
+         --argc;
+       }
+       else if (arg == "-o"){
+         ret.output_fn = (++argv)[0];
+         --argc;
+       }
+       else{
+         std::cerr << "Invalid argument: " << arg << std::endl;
+         usage(exec, std::cerr);
+         return ret;
+       }
+     }
+
+     if (ret.device_ids.size() == 0){
+       ret.device_ids = {0};
+     }
+
+     return ret;
+  }
+
 
 };
 
